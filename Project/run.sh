c752d18657f70c479141bf2df85426a949c1b394
Joker Poker <lihaoran@mail.ustc.edu.cn>
2016-05-22 06:45:28 +0800

    refine output information make the process traceable

--- Project/run.sh
+++ Project/run.sh
@@ -1,5 +1,21 @@
-echo ************Start Sompile**********
-make all
-echo ************Compile Done************
-cd bin 
-./formula_translator
+echo
+echo "********************Start Sompile*********************"
+echo
+res=$(make all)
+if [[ $? == 0 ]]; then
+	if [[ $res == *"Nothing to be done"* ]]; then
+		echo "All files are up to date."
+	else
+		echo $res
+	fi
+	echo
+	echo "*******************Compile Complete*******************"
+	echo
+	cd bin 
+	./formula_translator
+else
+	echo $res
+	echo
+	echo "***********Compile terminated with error(s)***********"
+	echo
+fi
